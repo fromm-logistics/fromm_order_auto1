096334eb82f82589c8bab3d39331ad2655d3cde2
fromm-logistics <junwoo.nam@wonderwall.kr>
2026-06-24 15:51:54 +0900

    Update invoice_main.py

--- invoice_main.py
+++ invoice_main.py
@@ -312,9 +312,9 @@ def run_invoice_main():
                                 st.write(list(df.columns))
                                 st.dataframe(df.head(5))
                             else:
-                                # 1. '판매처'에서 "wonderwall(해외)" 행 삭제
+                                # 1. '판매처'에서 "Wonderwall(해외)" 행 삭제
                                 if '판매처' in df.columns:
-                                    df = df[df['판매처'] != "wonderwall(해외)"]
+                                    df = df[df['판매처'] != "Wonderwall(해외)"]
 
                                 # 2. '수령자주소'에 "YTO(노머스 대련CC)" 포함하는 행 삭제
                                 if '수령자주소' in df.columns:
