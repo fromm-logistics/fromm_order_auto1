963e19741626a9f3890e34d1ef68dd77576b6c26
fromm-logistics <junwoo.nam@wonderwall.kr>
2026-09-17 17:26:33 +0900

    Update MD_SS.py

--- MD_SS.py
+++ MD_SS.py
@@ -5,7 +5,7 @@ import io, re, random
 import datetime
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
-from weight_db import load_weights  # ← 구글 시트 무게 DB
+from weight_db import load_weights, inject_floating_weight_btn  # ← 구글 시트 무게 DB
 
 # ───────────────────────────────────────────────────
 # 1) 유저가 수정·확장 가능한 영역:
@@ -245,6 +245,7 @@ exclude_products = ['[온유 팝업_TOUGH LOVE] 페이퍼 인센스 특전 포�
 
 
 def run_md_ss():
+    inject_floating_weight_btn()
     st.button("◀ MD 창으로 돌아가기",
               on_click=lambda: st.session_state.update(page="md_main"),
               key="back_to_md_main_from_FS")
