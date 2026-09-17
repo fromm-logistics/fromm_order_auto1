42e9bff478bf80be14eb5b4448901b6f9826c73b
fromm-logistics <junwoo.nam@wonderwall.kr>
2026-09-17 17:30:34 +0900

    Update weight_db.py

--- weight_db.py
+++ weight_db.py
@@ -104,7 +104,7 @@ def inject_floating_weight_btn():
 <style>
 .floating-weight-btn {{
     position: fixed;
-    right: 160px;
+    right: 320px;
     top: 50%;
     transform: translateY(-50%);
     z-index: 9999;
